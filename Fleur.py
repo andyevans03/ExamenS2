48257de99cd3ac0cc0ff09b55a874029005beada
rakot <rakot@USER>
2025-04-30 13:26:18 +0200

    Creation de la classe fleur

---
+++ Fleur.py
@@ -0,0 +1,8 @@
+class Fleur:
+    def __init__(self, espece, identifiant_fleur, date_coupe, qualite_fleur, prix_fleur):
+        
+        self.espece = espece
+        self.identifiant_fleur = identifiant_fleur
+        self.date_coupe = date_coupe
+        self.qualite_fleur = qualite_fleur
+        self.prix_fleur = prix_fleur
