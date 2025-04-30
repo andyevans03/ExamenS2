8ec488c0dbb7f7f0f8418ae631e0b89b6a55c01c
rakot <rakot@USER>
2025-04-30 15:01:33 +0200

    Ajustements dans les classes

--- Facture.py
+++ Facture.py
@@ -1,12 +1,20 @@
 from Fleur import Fleur
+from datetime import datetime
 
 class Facture:
     def __init__(self, nom_client, identifiant_facture, date_vente, fleur:list[Fleur], prix_vente):
+        
         if prix_vente < 0: ## le prix de vente ne peut pas etre négatif
             raise ValueError("Le prix ne peut pas être négatif.")
         
         self.nom_client = nom_client
         self.identifiant_fleur = identifiant_facture
-        self.date_vente = date_vente
+        self.date_vente =  datetime.strptime(date_vente, "%Y-%m-%d")
         self.fleur = fleur 
         self.prix_vente = prix_vente
+        
+    def valider_date_vente(self):
+        if self.date_vente < self.fleur.date_coupe:
+         raise ValueError("La date de vente ne peut pas être antérieure à la date de coupe des fleurs.")
+        
+    
